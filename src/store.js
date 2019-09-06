1fced2c600ace4d72e30098377d3ffe1387f31a3
Wang-Hui-Liu <54693676+Wang-Hui-Liu@users.noreply.github.com>
2019-09-06 21:23:33 +0800

    Update store.js

--- src/store.js
+++ src/store.js
@@ -8,11 +8,14 @@ export default new Vuex.Store({
      list:[]
   },
   mutations: {
+    //给购物车里的list赋值
      getList(state,payload){
+       //找要添加到购物车里的数据的索引
       let index=state.list.findIndex(item=>item.id==payload.id)
       if(index===-1){
         state.list.push(payload);
       }else{
+        //如果index！=-1，找到对应的数据改变它的count值
         state.list.filter(item=>{
           if(item.id==payload.id*1){
             item.count++
@@ -21,7 +24,9 @@ export default new Vuex.Store({
         })
       }
      },
-     changeCount(state,payload){
+    //改变count
+     changeCount(state,payload){  
+         //直接返回值        
          state.list[payload.index].count=payload.count
      }
   },
